66cfa71c0c16d627620580a12e0afa590ebf4eba
UdayBanda1147 <164988740+UdayBanda1147@users.noreply.github.com>
2024-04-19 00:26:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function Login() {
+  alert("Thank You! Your Logged In successfully Thanks!");
+}
+
+function Added() {
+  alert("Thank You! Your Item is Added to Cart successfully Thanks!");
+}
+
+function openForm() {
+  document.getElementById("hero");
+  alert("Redirecting a user to the Login Page!");
+}
+
+function closeForm() {
+  document.getElementById("signup-form");
+  alert("Thank You! Your details has been successfully submitted Thanks!");
+}
+
